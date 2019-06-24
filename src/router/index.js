d58c75f6c1e2e48b0e5f78e72ee8199e4431d328
hao <2595004299@qq.com>
2019-06-24 08:57:51 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ export default new Router({
           component: () => import('@/views/publish')
         }
       ]
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: () => import('@/views/login')
     }
   ]
 })
