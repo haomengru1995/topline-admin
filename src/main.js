a3af27ceddf56142b4cae293bc49831c3d8d40b9
hao <2595004299@qq.com>
2019-06-27 17:20:48 +0800

    接口访问权限

--- src/main.js
+++ src/main.js
@@ -6,9 +6,26 @@ import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
 import 'nprogress/nprogress.css'
 import axios from 'axios'
+import { getUser } from '@utils/auth'
 
 // axios.defaults.baseURL = 'http://toutiao.course.itcast.cn/mp/v1_0'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+
+axios.interceptors.request.use(config => {
+  const user = getUser()
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, error => {
+  return Promise.reject(error)
+})
+
+axios.interceptors.response.use(response => {
+  return response
+}, error => {
+  return Promise.reject(error)
+})
 
 Vue.use(ElementUI)
 
